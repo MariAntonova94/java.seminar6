0c69b39150c5c6f45f2a1ccaab7f91a8fbaf7f8e
Мария <114505489+MariAntonova94@users.noreply.github.com>
2023-03-12 17:49:28 +0700

    Add files via upload

---
+++ Catalog.java
@@ -0,0 +1,13 @@
+package org.example.seminar6.cw6;
+
+import org.example.seminar6.cw6.Laptop;
+
+import java.util.*;
+
+public class Catalog extends Laptop {
+
+    public Catalog(int id, int ram, int ssd, String os, String color){
+        super(id,ram,ssd,os,color);
+    }
+ 
+}
